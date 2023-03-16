87b9a79eb2cbb9403814190b3f5772026a0c6884
Masonwilcox <Masonwilcox17@gmail.com>
2023-03-16 12:19:21 -0400

    maybe the email works

--- send-email.php
+++ send-email.php
@@ -1,19 +1,39 @@
 <?php
-    $email = $_POST['email'];
-    $to = $email;
-    $cc = "queensupipeband@gmail.com";
-    $subject = "Class Signup Confirmation";
-    $message = "Thank you " . $name . " for signing up for our " . $level . " class. We will contact you at " . $email . " to confirm your class.";
-    $headers = "From: queensupipeband@gmail.com" . "\r\n" ."CC: $cc";
-    mail($to, $subject, $message, $headers);
 
-    require "vendor/autoload.php";
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
+require "vendor/autoload.php";
 
-    $mail = new PHPMailer(true);
-    
-    ?>
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.example.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 2525;
+
+$mail->Username = "queensupipeband@gmail.com";
+$mail->Password = "9227B3B17331E9E82B1A0F7B307C41EE0483";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("queensupipeband@example.com", "Queens Band");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
+
+?>
 
     
